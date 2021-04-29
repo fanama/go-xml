73a292671aeda9bec41263a7fb6c4d5577ed0151
fanama <fana.rakotoasimbola@clirisgroup.com>
2021-04-29 09:38:11 +0200

    update with ID + version

--- cmd/variables/type.go
+++ cmd/variables/type.go
@@ -4,12 +4,18 @@ import "encoding/xml"
 
 type Xlif struct {
 	XMLName xml.Name `xml:"xliff"`
+	Version string   `xml:"version,attr"`
 	File    File     `xml:"file"`
 }
 
 type File struct {
-	XMLName xml.Name `xml:"file"`
-	Body    Body     `xml:"body"`
+	XMLName  xml.Name `xml:"file"`
+	Original string   `xml:"original,attr"`
+	Source   string   `xml:"source-language,attr"`
+	Target   string   `xml:"target-language,attr"`
+	Type     string   `xml:"translation-type,attr"`
+	Datatype string   `xml:"datatype,attr"`
+	Body     Body     `xml:"body"`
 }
 
 type Body struct {
@@ -18,7 +24,10 @@ type Body struct {
 }
 
 type Trans struct {
-	XMLName xml.Name `xml:"trans-unit"`
-	Source  string   `xml:"source"`
-	Target  string   `xml:"target"`
+	XMLName  xml.Name `xml:"trans-unit"`
+	ID       string   `xml:"id,attr"`
+	MaxWidth string   `xml:"maxwidth,attr"`
+	SizeUnit string   `xml:"size-unit,attr"`
+	Source   string   `xml:"source"`
+	Target   string   `xml:"target"`
 }
